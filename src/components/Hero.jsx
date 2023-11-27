d1f42a3c1337aff5f52b5c132298e8347be24749
Adinath Shelke <shelkeadinath3@gmail.com>
2023-11-27 12:05:23 +0530

    hero text section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='hero-section flex justify-center items-center'>
-        <div className="hero-text w-[50%]  h-[100%] flex items-center text-black flex justify-center">
-            <div className="text w-[85%] flex justify-center text-[22px]">
-            <span>Toronto Cupcake: Canada's top choice for delectable treats! Order online 24/7 for events or just because, celebrating 13 years of delighting with the finest, freshly baked cupcakes. Enjoy delivery from 8 am to 6 pm TO time, bringing our signature pink box of sweetness to Toronto, the GTA, and beyond.</span>
+        <div className="hero-text w-[50%]  h-[100%] flex items-center text-black flex justify-center flex-col">
+            <div className="text w-[85%] flex justify-center text-[22px] font-black	 flex-col items-center">
+             <span>Toronto Cupcakes</span>  <span>Crafting Sweet Memories, One Bite at a Time!</span>
+            </div>
+            <div className="explore-btn mt-[1rem]">
+                <button className="bg-[#d441ed] hover:bg-[#f00] text-white font-bold py-2 px-4 rounded-full mt-[0.6rem]">
+                    Explore Delicious Cup-Cakes!
+                </button>
             </div>
         </div>
         <div className="hero-logo w-[50%]  h-[100%]"></div>
